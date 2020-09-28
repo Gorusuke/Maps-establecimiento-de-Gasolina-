55c329e9583189c0f89c0669655a5b829790e876
Jose Andres <Gorusuke123@gmail.com>
2020-09-27 19:33:35 -0500

    Establecimientos de gasolina

---
+++ js/API.js
@@ -0,0 +1,14 @@
+class API{
+    async obtenerDatos(){
+
+        const total = 1000;
+        // obtener datos desde la api
+        const datos = await fetch(`https://api.datos.gob.mx/v1/precio.gasolina.publico?pageSize=${total}`);
+
+        // retornar datos con json
+        const respuestaJson = await datos.json();
+
+        return{respuestaJson}
+    }
+
+}
---
+++ js/UI.js
@@ -0,0 +1,85 @@
+class UI {
+    constructor() {
+
+        // instanciar api
+        this.api = new API();
+
+        // crear los markers con layerGroup
+        this.markers = new L.LayerGroup();
+
+        // Iniciar el mapa
+        this.mapa = this.inicializarMapa();
+
+    }
+
+    inicializarMapa() {
+        // Inicializar y obtener la propiedad del mapa
+        const map = L.map('mapa').setView([19.390519, -99.3739778], 6);
+        const enlaceMapa = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
+        L.tileLayer(
+            'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; ' + enlaceMapa + ' Contributors',
+            maxZoom: 18,
+            }).addTo(map);
+        return map;
+
+    }
+
+    mostrarEstablecimientos(){
+        this.api.obtenerDatos()
+            .then((datos) => {
+                const resultado = datos.respuestaJson.results;
+                // ejecutar la funcion para mostrar los pines
+                this.mostrarPines(resultado)
+            })
+    }
+
+    mostrarPines(datos){
+        // limpiar los markers
+        this.markers.clearLayers();
+
+        // recorrer los establecimientos
+        datos.forEach(dato => {
+            // destructuring
+            const {latitude, longitude, calle, regular, premium} = dato;
+            // crear popup
+            const opcionesPopUp = L.popup().setContent(`
+                <p> Calle: ${calle} </p>
+                <p><strong> Regular:</strong> $${regular} </p>
+                <p><strong> Premium:</strong> $${premium} </p>
+            
+            
+            `)
+            
+            // agregar el pin
+            const marker = new L.marker([
+                parseFloat(latitude),
+                parseFloat(longitude)
+            ]).bindPopup(opcionesPopUp);
+            this.markers.addLayer(marker);
+        });
+        this.markers.addTo(this.mapa);
+    }
+
+    obtenerSugerencias(busqueda){
+        this.api.obtenerDatos()
+            .then((datos) => {
+                // obtener los datos
+                const resultados = datos.respuestaJson.results;
+
+                // enviar el Json y la busqueda para el filtrado
+                this.filtrarSugerencia(resultados, busqueda);
+
+            })
+    }
+
+    // filtra las sugerencias en base al input
+    filtrarSugerencia(resultado, busqueda){ 
+        // filtrar con .filter
+        const filtro = resultado.filter(filtro => filtro.calle.indexOf(busqueda) !== -1);
+
+        // mostrar los pines
+        this.mostrarPines(filtro);
+    }
+
+}
